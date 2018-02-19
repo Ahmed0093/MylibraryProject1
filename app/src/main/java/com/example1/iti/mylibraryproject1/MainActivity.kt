6db17ebd5e66cbc452ccc0cef4f2e2ba8fadcffc
Ahmed0093 <Ahmed0093@github.com>
2018-02-20 00:13:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example1/iti/mylibraryproject1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example1.iti.mylibraryproject1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example1.iti.libraryadd.*;
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ libraryadd/src/main/java/com/example1/iti/libraryadd/RssFeedProvider.java
@@ -0,0 +1,18 @@
+package com.example1.iti.libraryadd;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by Ahmed on 2/19/2018.
+ */
+
+public class RssFeedProvider {
+
+
+    public static int Add(int numb1,int numb2)
+    {
+        return numb1+numb2;
+    }
+}
