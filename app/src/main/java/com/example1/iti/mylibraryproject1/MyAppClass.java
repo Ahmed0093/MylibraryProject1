6c19321ea6ecaced216e297210df84d7c1ff8d26
Ahmed0093 <Ahmed0093@github.com>
2018-02-20 10:47:37 +0200

    Added Methods

---
+++ app/src/main/java/com/example1/iti/mylibraryproject1/MyAppClass.java
@@ -0,0 +1,17 @@
+package com.example1.iti.mylibraryproject1;
+
+/**
+ * Created by Ahmed on 2/20/2018.
+ */
+
+public class MyAppClass {
+    public static int myAppAdd(int numb1,int numb2)
+    {
+        return numb1+numb2;
+    }
+
+    public  int myAppsub(int numb1,int numb2)
+    {
+        return numb1-numb2;
+    }
+}
