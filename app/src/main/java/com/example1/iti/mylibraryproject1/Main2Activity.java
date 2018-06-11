9362de1b02219b19103ae8c06628017f3601b145
Ahmed0093 <Ahmed0093@github.com>
2018-06-11 02:04:57 +0200

    NAV BAR Adding Expandable

--- app/src/main/java/com/example1/iti/mylibraryproject1/Main2Activity.java
+++ app/src/main/java/com/example1/iti/mylibraryproject1/Main2Activity.java
@@ -2,13 +2,18 @@ package com.example1.iti.mylibraryproject1;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,22 +22,134 @@ import com.example1.iti.libraryadd.LibraryView;
 import com.example1.iti.libraryadd.MylibraryActivity;
 import com.example1.iti.libraryadd.RssFeedProvider;
 
-public class Main2Activity extends AppCompatActivity {
-
+public class Main2Activity extends AppCompatActivity implements LibraryView.MyClick,LibraryView.MyClickText{
+    private BottomNavigationView defaultBottomNavigationView, expandBottomNavigationView, moreMenuNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Button btn = (Button) findViewById(R.id.btntest);
+        defaultBottomNavigationView = (BottomNavigationView) findViewById(R.id.default_bottom_navigation);
+        expandBottomNavigationView = (BottomNavigationView) findViewById(R.id.expand_bottom_navigation);
+        final GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setVisibility(View.GONE);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(Main2Activity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
         Log.v("Result","abl al btn");
 
         LibraryView libraryView = (LibraryView) findViewById(R.id.mylibtest);
-    libraryView.setOnClickListener(new View.OnClickListener() {
+        Context context = libraryView.getCtx();
+       //Button btn2= (Button)findViewById(R.id.mylibrarybtn);
+       View myview=libraryView.getView();
+        Button btn2= (Button)myview.findViewById(R.id.mylibrarybtn);
+        myview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+Toast.makeText(getApplicationContext(),"fel view",Toast.LENGTH_LONG).show();
+            }
+        });
+        defaultBottomNavigationView.setVisibility(View.VISIBLE);
+        defaultBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
-        public void onClick(View view) {
-            Toast.makeText(getApplicationContext(),"library view clked",Toast.LENGTH_LONG).show();
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            item.setChecked(false);
+            switch (item.getItemId()) {
+                case R.id.action_home:
+                    //Check the Item
+                    item.setChecked(true);
+                    replaceDummyFragment(item);
+                    break;
+                case R.id.action_message:
+                    //Check the Item
+                    item.setChecked(true);
+                    replaceDummyFragment(item);
+                    break;
+                case R.id.action_notification:
+                    //Check the Item
+                    item.setChecked(true);
+                    defaultBottomNavigationView.setVisibility(View.GONE);
+                    expandBottomNavigationView.setVisibility(View.VISIBLE);
+                    gridview.setVisibility(View.VISIBLE);
+                    replaceDummyFragment(item);
+                    break;
+            }
+            return false;
         }
     });
+        expandBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(false);
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        //Check the Item
+                        item.setChecked(true);
+                        replaceDummyFragment(item);
+                        break;
+                    case R.id.action_message:
+                        //Check the Item
+                        item.setChecked(true);
+                        replaceDummyFragment(item);
+                        break;
+                    case R.id.action_notification:
+                        //Check the Item
+                        item.setChecked(true);
+                        defaultBottomNavigationView.setVisibility(View.VISIBLE);
+                        expandBottomNavigationView.setVisibility(View.GONE);
+                        gridview.setVisibility(View.GONE);
+
+                        replaceDummyFragment(item);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        //        libraryView.setOnEventListener(new LibraryView.OnEventListener() {
+//            @Override
+//            public void onEvent() {
+//                Toast.makeText(getApplicationContext(),"fel view event listener ",Toast.LENGTH_LONG).show();
+//
+//            }
+//        });
+//
+//        libraryView.doEvent();
+
+
+        //libraryView.onclick12(btn2);
+       // Log.v("Result","abl khales library");
+
+       // libraryView.librarybtn();
+        //libraryView.onclick12();
+        //libraryView.
+
+
+//        btn2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.v("Result","fell al btn 2 ");
+//
+//            }
+//        });
+//       btn2.setOnClickListener(new View.OnClickListener() {
+//           @Override
+//           public void onClick(View view) {
+//               Log.v("Result","Clicked");
+//           }
+//       });
+//    libraryView.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            Toast.makeText(getApplicationContext(),"library view clked",Toast.LENGTH_LONG).show();
+//        }
+//    });
         //LibraryView libraryView1 = new LibraryView(this.getApplication());
         // inflate (create) another copy of our custom layout
       //  LinearLayout mainLayout = (LinearLayout) findViewById(R.id.ac);
@@ -55,5 +172,21 @@ public class Main2Activity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void replaceDummyFragment(MenuItem item) {
+        Toast.makeText(getApplicationContext(),"Text Clicked"+item.toString(),Toast.LENGTH_LONG).show();
+
+    }
+
+
+    @Override
+    public void onclick12(View view) {
+        Toast.makeText(getApplicationContext(),"BTN Clicked",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onclicktext(View view) {
+        Toast.makeText(getApplicationContext(),"Text Clicked",Toast.LENGTH_LONG).show();
     }
 }
--- libraryadd/src/main/java/com/example1/iti/libraryadd/LibraryView.java
+++ libraryadd/src/main/java/com/example1/iti/libraryadd/LibraryView.java
@@ -5,12 +5,15 @@ import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -19,28 +22,95 @@ import java.util.Calendar;
  * Created by Ahmed on 2/20/2018.
  */
 
-public class LibraryView extends LinearLayout {
+public class LibraryView extends LinearLayout{
     private LinearLayout header;
-    static Button btnPrev;
-//    private ImageView btnNext;
+    protected Button btnPrev;
+//    private LibraryView.OnMyClickListener j;
+//    private OnEventListener mOnEventListener;
+onAddTextViewCustomListener onAddTextViewCustomListener; //listener custom
+
+    public interface onAddTextViewCustomListener  {
+        //
+    }
+
+    //    private ImageView btnNext;
     private TextView txtDate;
 //    private GridView grid;
+    private OnClickListener l;
+    private  Context ctx;
+ private View view;
+
+    public Context getCtx() {
+        return ctx;
+    }
 
     public LibraryView(Context context) {
         super(context);
         initControl(context);
-
     }
 
+
+    //    public void setOnEventListener(OnEventListener listener) {
+//        mOnEventListener = listener;
+//    }
+//    public void doEvent() {
+//        /*
+//         * code code code
+//         */
+//  //      btnPrev.setOnClickListener(l.onClick(););
+//        btnPrev.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        }});
+//
+//        // and in the end
+//
+//        if (mOnEventListener != null)
+//            mOnEventListener(); // event result object :)
+//    }
     private void initControl(Context context) {
         LayoutInflater inflater = (LayoutInflater)
                 context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.mylibraryview, this);
+
+      view=  inflater.inflate(R.layout.mylibraryview,this);
         header = (LinearLayout)findViewById(R.id.linearlayoutid);
         btnPrev = (Button) findViewById(R.id.mylibrarybtn);
         txtDate =(TextView) findViewById(R.id.mylibtextid);
+        this.ctx=context;
+    }
+        public View getView()
+    {
+        return view;
+    }
+    public void librarybtn()
+    {
+        Toast.makeText(ctx,"fel view",Toast.LENGTH_LONG).show();
+        Log.v("Result","fe btn al library");
+    }
+    public void onclick12212()
+    {
+        Toast.makeText(ctx,"fel view",Toast.LENGTH_LONG).show();
+        Log.v("Result","fe btn al library");
     }
 
+    private void c() {
+        this.btnPrev.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("Result","fe btn al library al gdeed");
+            }
+        });
+    }
+
+
+
+    public Button getbtnview(Context context)
+    {
+        return (Button) findViewById(R.id.mylibrarybtn);
+
+    }
     public LibraryView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         initControl(context);
@@ -64,25 +134,86 @@ public class LibraryView extends LinearLayout {
 
     }
 
+    @Override
+    public View getRootView() {
+        LayoutInflater inflater = (LayoutInflater)
+                ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+       View view= inflater.inflate(R.layout.mylibraryview, this);
+        return view;
+    }
+
     private void setView() {
         // has the format hour.minuits am/pm
         SimpleDateFormat dateFormat = new SimpleDateFormat("hh.mm aa");
         String time = dateFormat.format(Calendar.getInstance().getTime());
-
+ //           OnMyClickListener onMyClickListener = null;
+//            onMyClickListener.onMyviewClick(this.txtDate);
         if(this.txtDate != null )
             txtDate.setText("TEST "+" "+time);
         else
             this.txtDate.setText("TEST BAS");
     }
 
+
     @Override
     public void setLongClickable(boolean longClickable) {
         super.setLongClickable(longClickable);
+        btnPrev.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
-
-
-    public interface MytestListener
+    public interface MyClick
     {
-        void onbtnclickedlistener(Button btn);
+        public void onclick12(View view);
+
     }
+    public interface MyClickText
+    {
+        public void onclicktext(View view);
+
+    }
+    public interface Mylistener
+    {
+        //public void onclicklistener(View view);
+
+    }
+
+
+
+
+
+//    //...................
+//    private void b() {
+//        this.c();
+//
+//                this.d.setImageResource(this.m);
+//                this.d.setOnClickListener(new OnClickListener() {
+//                    public void onClick(View var1) {
+//                        if(VfgClickCell.this.j != null) {
+//                            VfgClickCell.this.j.onClick();
+//                        }
+//
+//                    }
+//                });
+//
+//
+//        }
+//
+//    }
+    //................
+//    public void onclick12(View var)
+//    {
+//        Toast.makeText(ctx,"fel viewbtn12",Toast.LENGTH_LONG).show();
+//        Log.v("Result","fe btn al library122");
+//
+//    }
+
+
+
+
 }
+
+
